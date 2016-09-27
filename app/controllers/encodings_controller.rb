94d603a93e833909adfacb1e2153fa37fd33a145
Melissa Rodriguez <olenellina@gmail.com>
2016-09-27 16:11:13 -0700

    working v1

---
+++ app/controllers/encodings_controller.rb
@@ -0,0 +1,13 @@
+class EncodingsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
+  def index
+  end
+
+  def new
+  end
+
+  def show
+    @message = Caesar.encode(params[:shift], params[:message])
+  end
+end
